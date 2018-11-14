e1fb9b0d884af8c83033fbd24d56d4c428e29b8e
kerinb <kerinb@tcd.ie>
2018-11-14 17:35:06 +0000

    edit run_trec_eval.sh

--- run_trec_eval.sh
+++ run_trec_eval.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-"$(pwd)"/trec_eval/trec_eval -m all_trec "$(pwd)"/cran/cranqrel "$(pwd)"/cran/cranQueryResults > "$(pwd)"/output.txt
+"$(pwd)"/trec_eval/trec_eval -m all_trec "$(pwd)"/qrels.assignment2A "$(pwd)"/DataSet/querryResults > "$(pwd)"/output.txt
+
