ba59ee1456296399cf8329cded1cafc9e6f614b1
kerinb <kerinb@tcd.ie>
2018-11-16 10:35:50 +0000

    midpoint evaluation - MAP = 0.2495!

--- run_trec_eval.sh
+++ run_trec_eval.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-"$(pwd)"/trec_eval/trec_eval -m all_trec "$(pwd)"/qrels.assignment2A "$(pwd)"/DataSet/querryResults > "$(pwd)"/output.txt
+"$(pwd)"/trec_eval/trec_eval -m all_trec "$(pwd)"/DataSet/qrels.assignment2.part1 "$(pwd)"/DataSet/queryResults > "$(pwd)"/output.txt
 
