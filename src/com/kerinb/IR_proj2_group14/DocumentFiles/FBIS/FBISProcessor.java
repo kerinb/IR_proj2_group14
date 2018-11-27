f5d1384a4200e9a00ceeadeee3d7d80a2ddb9387
Sinead Dickson <sdickson@tcd.ie>
2018-11-27 16:54:13 +0000

    removed date, added boolean query, map 0.2907

--- src/com/kerinb/IR_proj2_group14/DocumentFiles/FBIS/FBISProcessor.java
+++ src/com/kerinb/IR_proj2_group14/DocumentFiles/FBIS/FBISProcessor.java
@@ -48,42 +48,8 @@ public class FBISProcessor {
 			
 			FBISData fbisData = new FBISData();
 			if(doc.getElementsByTag(FBISTags.DOCNO.name()) != null) 	fbisData.setDocNum(trimData(doc, FBISTags.DOCNO));
-			if(doc.getElementsByTag(FBISTags.HEADER.name()) != null)	fbisData.setHeader(trimData(doc, FBISTags.HEADER));
 			if(doc.getElementsByTag(FBISTags.TEXT.name()) != null) 		fbisData.setText(trimData(doc, FBISTags.TEXT));
             if(doc.getElementsByTag(FBISTags.TI.name()) != null) 		fbisData.setTi(trimData(doc, FBISTags.TI));
-
-            if(doc.getElementsByTag(FBISTags.DATE1.name()) != null){
-
-				date = trimData(doc, FBISTags.DATE1).trim();
-				day = date.substring(0,2).trim();
-				if(day.length()==1) day = "0" + day;
-				month = date.substring(2,date.length()-4).trim();
-				if(date.endsWith("*")) year = date.substring(date.length()-3,date.length()-1).trim();
-				else year = date.substring(date.length()-2,date.length()).trim();
-
-				if(date.startsWith("M")) {
-					month = date.substring(0,date.length()-7).trim();
-					day = date.substring(date.length()-7,date.length()-4).trim();
-					year = date.substring(date.length()-2,date.length()).trim();
-				}
-
-				if(month.startsWith("Jan")) month = "01";
-				else if(month.startsWith("Feb")) month = "02";
-                else if(month.startsWith("Mar")|| month.startsWith("MAR")) month = "03";
-                else if(month.startsWith("Apr")) month = "04";
-                else if(month.startsWith("May")) month = "05";
-                else if(month.startsWith("Jun")) month = "06";
-                else if(month.startsWith("Jul")) month = "07";
-                else if(month.startsWith("Aug")) month = "08";
-                else if(month.startsWith("Sep")) month = "09";
-                else if(month.startsWith("Oct")) month = "10";
-                else if(month.startsWith("Nov")) month = "11";
-                else if(month.startsWith("Dec")) month = "12";
-
-				date = year + month + day;
-
-				fbisData.setDate(date);
-			}
 
 			fbisDocList.add(createFBISDocument(fbisData));
 		}
@@ -121,13 +87,7 @@ public class FBISProcessor {
 
 	private static Document createFBISDocument(FBISData fbisData) {
         Document document = new Document();
-        int date = 0;
-		if(fbisData.getDate().chars().allMatch(Character::isDigit)) {
-			date = Integer.valueOf(fbisData.getDate());
-		}
-
         document.add(new StringField("docno", fbisData.getDocNum(), Field.Store.YES));
-       	document.add(new IntPoint("date", date));
         document.add(new TextField("headline", fbisData.getTi(), Field.Store.YES));
         document.add(new TextField("text", fbisData.getText(), Field.Store.YES));
 
