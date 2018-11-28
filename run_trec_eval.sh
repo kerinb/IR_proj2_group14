a75bb1757136fc2cc997acd4c2529a391ab3606a
kerinb <kerinb@tcd.ie>
2018-11-28 14:57:30 +0000

    merge prep

--- run_trec_eval.sh
+++ run_trec_eval.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-"$(pwd)"/trec_eval.9.0/trec_eval -m all_trec "$(pwd)"/DataSet/qrels.assignment2.part1 "$(pwd)"/DataSet/queryResults > "$(pwd)"/output.txt
+"$(pwd)"/trec_eval.9.0/trec_eval -m all_trec "$(pwd)"/qrels "$(pwd)"/DataSet/queryResults > "$(pwd)"/output.txt
