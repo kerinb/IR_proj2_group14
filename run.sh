7cb7be72c020ca9eabbc60cf9adaa17f7ae72d25
Kayleigh McGinley <kmcginle@tcd.ie>
2018-11-29 22:37:28 +0000

    revert to 'merge pull request #7' (relevant/non-relevant) - 10 days ago

--- run.sh
+++ run.sh
@@ -1,41 +1,41 @@
 #!/usr/bin/env bash
 #/bin/bash
 
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 MORFOLOGIC
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BOOLEAN MORFOLOGIC
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC MORFOLOGIC
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET MORFOLOGIC
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT MORFOLOGIC
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 MORFOLOGIC
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BOOLEAN MORFOLOGIC
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC MORFOLOGIC
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET MORFOLOGIC
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT MORFOLOGIC
 
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 STOP
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BOOLEAN STOP
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC STOP
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET STOP
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT STOP
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 STOP
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BOOLEAN STOP
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC STOP
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET STOP
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT STOP
 
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 SIMPLE
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BOOLEAN SIMPLE
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC SIMPLE
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET SIMPLE
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT SIMPLE
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 SIMPLE
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BOOLEAN SIMPLE
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC SIMPLE
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET SIMPLE
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT SIMPLE
 
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 STANDARD
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BOOLEAN STANDARD
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC STANDARD
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET STANDARD
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT STANDARD
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 STANDARD
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BOOLEAN STANDARD
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC STANDARD
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET STANDARD
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT STANDARD
 
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 WHITESPACE
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.ja*r BOOLEAN WHITESPACE
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC WHITESPACE
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET WHITESPACE
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT WHITESPACE
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 WHITESPACE
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.ja*r BOOLEAN WHITESPACE
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC WHITESPACE
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET WHITESPACE
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT WHITESPACE
 
-java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 CUSTOM # Best so far!
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BOOLEAN CUSTOM
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC CUSTOM
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET CUSTOM
-#java -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT CUSTOM
+java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BM25 CUSTOM # Best so far!
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar BOOLEAN CUSTOM
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar CLASSIC CUSTOM
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar LM_DIRICHLET CUSTOM
+#java -Xmx6g -jar "$(pwd)"/target/cranfield_application-0.0.1-SNAPSHOT.jar SWEET_SPOT CUSTOM
 
 
 
